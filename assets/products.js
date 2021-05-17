31149e06995247155a8bf9eb339d307cdde43791
Damián <damianrossi@live.com.ar>
2021-05-17 21:04:49 +0200

    Created minor mixins, colors, imported fonts and started main layout

---
+++ assets/products.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+const heroes = [
+  {
+    id: 1, name: 'Goku POP', Price: 5.00, code: 'GOKU'
+  },
+  {
+    id: 2, name: 'Naruto POP', Price: 5.00, code: 'NARU'
+  },
+  {
+    id: 3, name: 'Luffy POP', Price: 5.00, code: 'LUF'
+  }
+];
